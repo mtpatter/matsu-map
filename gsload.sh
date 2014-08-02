d305d9a31dd0b82deb3829cf244cce545f6d43fa
nikolasa <nikolasa@users.noreply.github.com>
2014-08-02 07:03:11 -0500

    Restructured for free naming of vector layer

--- gsload.sh
+++ gsload.sh
@@ -52,16 +52,16 @@ for file in "$@"; do
     yyyy=${name:10:4}
     ddd=${name:14:3}
     # Trick from http://superuser.com/a/232106
-    month=$(date -d "$yyyy-01-01 +$ddd days -1 day" "+%Y-%m")
+    month=$(date -d "${yyyy}-01-01 +${ddd} days -1 day" "+%Y-%m")
 
-    location="$gsdata/$workspace/$instr/$analytic/$month/$layer"
+    location="${gsdata}/${workspace}/${instr}/${instr}_${analytic}/${month}/${layer}"
 
     # Move the image and reproject it.
     sudo -u tomcat7 mkdir -p "${location}"
-    sudo -u tomcat7 rsync "$file" "$location/$layer.origproj" 
-    sudo -u tomcat7 rm -f "$location/$layer.tif"
-    sudo -u tomcat7 gdalwarp -t_srs EPSG:4326 "$location/$layer.origproj" "$location/$layer.tif"
-    sudo -u tomcat7 rm "$location/$layer.origproj"
+    sudo -u tomcat7 rsync "${file}" "${location}/${layer}.origproj" 
+    sudo -u tomcat7 rm -f "${location}/${layer}.tif"
+    sudo -u tomcat7 gdalwarp -t_srs EPSG:4326 "${location}/${layer}.origproj" "${location}/${layer}.tif"
+    sudo -u tomcat7 rm "${location}/${layer}.origproj"
 
     # This was a failed attempt at only copying and reprojecting if changed.
     #if [[ $(sudo -u tomcat7 rsync -ci "$file" "$location/$layer.badprojection" | wc -l) -ne 0 ]]; then
@@ -82,7 +82,7 @@ for file in "$@"; do
         curl -sSf -u $feeduser:$feedpass -XPOST -H 'Content-Type: application/atom+xml' -d "$(python ${BASH_SOURCE%/*}/feedEntry.py "$file" "$description")" http://localhost:8080/atomhopper/geoserver/feed.xml > /dev/null
     fi
 
-    # Set description of image (came from daily_reports)
+    # Set description of image (came from meta/daily_yyyy_ddd)
     curl -sSf -u $username:$password -XPUT -H 'Content-type: application/xml' -d "<coverage><title>$description</title><enabled>true</enabled></coverage>" http://localhost:8080/geoserver/rest/workspaces/$workspace/coveragestores/$layer/coverages/$layer.xml > /dev/null
 
     # Set style of image. (If it doesn't exist, it will remain the default.)
@@ -91,10 +91,21 @@ done
 
 # Rebuild ImageMosaic of all images from this instrument and analytic.
 # NOTE: If you include multiple analytics in a single gsload, only one updates.
-sudo -u tomcat7 rm -f $gsdata/$workspace/$instr/$analytic/$analytic.shp
+sudo -u tomcat7 rm -f ${gsdata}/${workspace}/${instr}/${instr}_${analytic}/${instr}_${analytic}.shp
 
-curl -sSf -u $username:$password -XPUT -H 'Content-type: text/plain' -d "file://$gsdata/$workspace/$instr/$analytic" http://localhost:8080/geoserver/rest/workspaces/$workspace/coveragestores/${instr}_${analytic}/external.imagemosaic?coverageName=${instr}_${analytic} > /dev/null
+curl -sSf -u $username:$password -XPUT -H 'Content-type: text/plain' -d "file://$gsdata/$workspace/${instr}/${instr}_$analytic" http://localhost:8080/geoserver/rest/workspaces/$workspace/coveragestores/${instr}_${analytic}_raster/external.imagemosaic?coverageName=${instr}_${analytic}_raster > /dev/null
 
-curl -sSf -u $username:$password -XPUT -H 'Content-type: text/xml' -d "<layer><defaultStyle><name>$analytic</name></defaultStyle></layer>" http://localhost:8080/geoserver/rest/layers/$workspace:${instr}_${analytic} > /dev/null
+curl -sSf -u $username:$password -XPUT -H 'Content-type: text/xml' -d "<layer><defaultStyle><name>$analytic</name></defaultStyle></layer>" http://localhost:8080/geoserver/rest/layers/$workspace:${instr}_${analytic}_raster > /dev/null
 
-curl -sSf -u $username:$password -XPUT -H "Content-type: text/plain" -d "file://$gsdata/$workspace/$instr/$analytic/$analytic.shp" http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores/${instr}_${analytic}_vector/external.shp > /dev/null
+# Build vector data from ImageMosaic shapefile
+curl -v -u "${username}:${password}" -XPUT -H "Content-type: text/plain" -d "file://${gsdata}/${workspace}/${instr}/${instr}_${analytic}/${instr}_${analytic}.shp" "http://localhost:8080/geoserver/rest/workspaces/${workspace}/datastores/${instr}_${analytic}/external.shp" #> /dev/null
+
+# We have to recalculate the bbox because of this:  https://github.com/geoserver/geoserver/blob/2.5.x/src/restconfig/src/main/java/org/geoserver/catalog/rest/DataStoreFileResource.java#L411
+#curl -sSf -u "${username}:${password}" -XPUT -H "Content-type: application/xml" -d "<featureType><name>${instr}_${analytic}</name><enabled>true</enabled></featureType>" "http://localhost:8080/geoserver/rest/workspaces/${workspace}/datastores/${instr}_${analytic}/featuretypes/${instr}_${analytic}.xml&recalculate=nativebbox,latlonbbox" > /dev/null
+
+#curl -v -u $username:$password -XPOST -H "Content-type: application/xml" -d @featuretype.xml "http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores/${instr}_${analytic}_vector/featuretypes.shp&recalculate=nativebbox,latlonbbox" #> /dev/null
+#<title>Vector data</title><enabled>true</enabled>
+#curl -v -u $username:$password -XPOST -H "Content-type: application/xml" -d "<featureType><name>${instr}_${analytic}_vector</name><attributes><attribute><name>the_geom</name><binding>com.vividsolutions.jts.geom.Point</binding></attribute><attribute><name>description</name><binding>java.lang.String</binding></attribute><attribute><name>timestamp</name><binding>java.util.Date</binding></attribute></attributes></featureType>" http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores/${instr}_${analytic}_vector/featuretypes.shp #> /dev/null
+#curl -v -u $username:$password -XPUT -H 'Content-type: application/xml' -d "<featureType><title>Vector data</title><enabled>true</enabled></featureType>" http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores/${instr}_${analytic}_vector/featuretypes/${instr}_${analytic}_vector.xml #> /dev/null
+#curl -v -u $username:$password -XPOST -H "Content-type: application/xml" -d "<featureType datastore=\"${instr}_${analytic}_vector\"><name>${instr}_${analytic}_vector</name><nativeName>${instr}_${analytic}_vector</nativeName><namespace><name>eo1</name></namespace><store class=\"dataStore\"><name>${instr}_${analytic}_vector</name></store><abstract>Vector data</abstract><title>Vector data</title><enabled>true</enabled></featureType>" http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores/${instr}_${analytic}_vector/featuretypes.shp #> /dev/null
+#curl -v -u $username:$password -XPUT -H "Content-type: text/plain" -d "file://$gsdata/$workspace/$instr/$analytic/$analytic.shp" http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores/${instr}_${analytic}_vector/featuretypes/${instr}_${analytic}_vector/external.shp #> /dev/null
